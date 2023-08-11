c2afcef5974a4ed869906a85631debb4cf037ba6
Poojitha Nuneti <nunetipoojitha@gmail.com>
2023-08-11 19:21:57 +0530

    add a program to revrse a string

---
+++ reverse.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cstring>
+using namespace std;
+int main()
+{
+string s;
+getline(cin,s);
+
+reverse(s.begin(),s.end());
+cout<<s;
+
+   
+    return 0;
+}
