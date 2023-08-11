02cd13750aff48326426b6f607ee5fa84035ea03
Poojitha Nuneti <nunetipoojitha@gmail.com>
2023-08-11 19:33:21 +0530

    correcting code allignment

--- reverse.cpp
+++ reverse.cpp
@@ -2,15 +2,14 @@
 #include<iostream>
 #include<bits/stdc++.h>
 #include<cstring>
+
 using namespace std;
+
 int main()
 {
-string s;
-getline(cin,s);
-
-reverse(s.begin(),s.end());
-cout<<s;
-
-   
+    string s;
+    getline(cin,s);
+    reverse(s.begin(),s.end());
+    cout<<s;
     return 0;
 }
